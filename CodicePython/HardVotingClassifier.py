566572570868af6e557dea2724857bc8555b30b3
Nicola Corea <51996551+Tesla97@users.noreply.github.com>
2023-05-13 13:46:33 +0200

    Update HardVotingClassifier.py

--- CodicePython/HardVotingClassifier.py
+++ CodicePython/HardVotingClassifier.py
@@ -1,6 +1,5 @@
 # Created by Nicola Corea , Software Engineer
 # Project For Machine Learning Course
-# 12 / 05 / 2023
 from sklearn.base import clone
 from sklearn.pipeline import _name_estimators
 import numpy as np
